360f716ff4d0b0459b612c46e21b10e5c722d1c2
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-07-29 12:47:01 +0530

    add ci.yml

---
+++ healthcare-system/src/main/java/com/yasiru/healthcare_system/HealthcareSystemApplication.java
@@ -0,0 +1,15 @@
+package com.yasiru.healthcare_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareSystemApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("************************* HEALTHCARE SYSTEM  *****************************");
+		SpringApplication.run(HealthcareSystemApplication.class, args);
+	}
+
+}
