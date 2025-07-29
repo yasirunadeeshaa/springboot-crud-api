cff10f150e83a71b0872d442494aa187c712341f
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-07-29 12:46:25 +0530

    create health test

---
+++ healthcare-system/src/test/java/com/yasiru/healthcare_system/HealthcareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasiru.healthcare_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
